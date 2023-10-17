fcd4b4f71b2f2ef109e3d4c05096f2b39f28c8e9
Lima <heinrichprog.66@gmail.com>
2023-10-17 15:28:23 +0200

    03

--- main/main.c
+++ main/main.c
@@ -6,11 +6,32 @@
 /*   By: lheinric <lheinric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 07:34:00 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/17 14:57:36 by lheinric         ###   ########.fr       */
+/*   Updated: 2023/10/17 15:24:12 by lheinric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void printenv(char **envp)
+{
+	int j;
+
+	j = -1;
+	while (envp[++j] != NULL)
+		printf("%s\n", envp[j]);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n - 1 && s1[i] == s2[i] && (s1[i] != 0 && s2[i] != 0))
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -21,9 +42,11 @@ int	main(int argc, char **argv, char **envp)
 	if (argc > 1)
 		return (printf("ERROR: usage ./minishell\n"), 1);
 	rl_initialize();
-	while (commande[0] != '1')
+	while (ft_strncmp("exit", commande, 5) != 0)
 	{
 		commande = readline("minishell : ");
+		if (ft_strncmp("env", commande, 4) == 0)
+			printenv(envp);
 	}
 	return 0;
 }
