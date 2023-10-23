e31bbdb6c30a27e3167b3ce9c0baeee3f093b0b3
Qcoudeyr <qcoudeyr@pm.me>
2023-10-23 07:17:04 +0000

    Oct 23, 2023, 9:17 AM

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 07:34:00 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/23 08:40:58 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/23 09:13:52 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,30 @@ void printenv(char **envp)
 		printf("%s\n", envp[j]);
 }
 
+int	print_header(char **envp)
+{
+	int		fd;
+	char	*str;
+	char	*args[2];
+
+	args[0] = "clear";
+	args[1] = NULL;
+	execve("/usr/bin/clear", args, envp);
+	
+	fd = open("./main/header", O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	str = get_next_line(fd);
+	while(str != NULL)
+	{
+		printf("%s",str);
+		free(str);
+		str = get_next_line(fd);
+	}
+	free(str);
+	return (0);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char *cmd;
@@ -29,6 +53,8 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc > 1)
 		return (printf("ERROR: usage ./minishell\n"), 1);
+	if (print_header(envp) == -1)
+		return (-1);
 	rl_initialize();
 	cmd = malloc(sizeof(char*));
 	while (ft_strncmp("exit", cmd, 4) != 0)
