162c3ce91c1085a51c9c85c6ff9545913ae35932
Lima-Heco <heinrichprog.66@gmail.com>
2023-10-25 12:27:31 +0200

    echo added

--- builtins/ft_heco.c
+++ builtins/ft_heco.c
@@ -6,29 +6,63 @@
 /*   By: lheinric <lheinric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 13:56:50 by lheinric          #+#    #+#             */
-/*   Updated: 2023/10/25 11:33:43 by lheinric         ###   ########.fr       */
+/*   Updated: 2023/10/25 12:24:24 by lheinric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*echo prend en charactere un fd (fichier ou sortie), la struct ms comme d'habitude 
+et un tableau de string le tableau de string doit commancer juste apres la string 
+echo appelee.*/
+
 #include "../minishell.h"
 
-echo_redirect
+void	find_bn(t_echo *echo, char **ordre, int i, int j)
+{
+	if (ordre[i+1] = NULL && (ordre[i][0] == '-'
+					&& ordre[i][1] == 'n'))
+				{
+					heco.nobackslash = 1;
+					heco.no_bs_position = i;
+				}
+			else if (strncmp(ordre[i-1], "echo") == 0 && (ordre[i][0] == '-'
+					&& ordre[i][1] == 'n'))
+				{
+					heco.nobackslash = 1;
+					heco.no_bs_position = i;
+				}
+}
 
-int	ft_heco(t_ms *t, char **ordre)
+void	print_echo(int fd, t_echo *echo, char **ordre, t_ms *t)
+{
+	int i;
+
+	i = -1
+	while(ordre[++i] != NULL)
+	{
+		if (i != heco.no_bs_position)
+			fd_printf(fd, "%s", ordre[i]);
+	}
+	if (echo.nobackslash == 0)
+		fd_printf(fd, "\n");
+}
+
+int	ft_echo(int fd, t_ms *t, char **ordre)
 {
 	int i;
 	int j;
+	t_echo echo;
 
-	t_heco heco;
-
+	echo.nobackslash = 0;
+	heco.no_bs_position = -1;
 	i = -1
 	while(ordre[++i] != NULL)
 	{
 		j = -1
 		while (ordre[i][++j])
 		{
-			if (ordre[i][0] == '-' && ordre[i][1] == 'n')
-				heco.nobackslash = 1;
+			find_bn(&echo, ordre, i, j);
 		}
 	}
+	print_echo(fd, &echo, ordre, t);
+	return (0);
 }
