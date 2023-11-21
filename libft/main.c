6908daa09cc13bcb6692550a680454fde5b93ade
Qcoudeyr <qcoudeyr@pm.me>
2023-11-21 09:04:27 +0000

    Nov 21, 2023, 10:04 AM

--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 09:53:15 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/11/21 09:54:49 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/11/21 10:03:15 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ int	main(void)
 
 	test = ft_splitq("echo \"ceci est un test\"");
 	for (int i = 0; test[i] != 0; i++)
-	{
 		printf("%s\n",test[i]);
-		free(test[i]);
-	}
-	free(test);
 	return 0;
 }
 
