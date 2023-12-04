de885368e2f88ace5c122e1c298aecc341608b2a
Qcoudeyr <qcoudeyr@pm.me>
2023-12-04 10:22:55 +0000

    Dec 4, 2023, 11:22 AM

--- utils/env_var.c
+++ utils/env_var.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:31:40 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/04 11:20:25 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/04 11:21:54 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,12 +55,32 @@ char	*handle_env_var(t_ms *t, char *str)
 	e.newstr = ft_calloc(ft_strlen(str) * 100, sizeof(char));
 	e.var = ft_calloc(100000, sizeof(char));
 	change_str_env(&e, t, str);
-	e.newstr[t->j] = 0;
 	free(e.var);
 	free(str);
 	return (e.newstr);
 }
-void	change_str_env(t_env e, )
+
+void	change_str_env(t_env *e, t_ms *t, char *str)
+{
+	while (str[t->i] != '\0')
+	{
+		hev_quote(str[t->i], &e->squote, &e->quote);
+		if ((e->quote == 1 || (e->squote == 0 && e->quote == 0)) && \
+str[t->i] == '$' && ft_isalpha(str[t->i + 1]))
+		{
+			while (ft_isalpha(str[++t->i]) != 0)
+				e->var[e->len++] = str[t->i];
+			e->var[e->len] = 0;
+			e->newstr = ft_strjoin(e->newstr, env_var(t, e->var));
+			free(e->var);
+			e->var = ft_calloc(1000, sizeof(char));
+			t->j = ft_strlen(e->newstr);
+		}
+		if (str[t->i] != 0)
+			e->newstr[t->j++] = str[t->i++];
+	}
+	e.newstr[t->j] = 0;
+}
 
 void	hev_quote(char c, int *squote, int *quote)
 {
