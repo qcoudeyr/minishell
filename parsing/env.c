b5576543a583fc42c4eff0f73c4b518c18faf238
Qcoudeyr <qcoudeyr@pm.me>
2023-12-04 10:10:48 +0000

    Dec 4, 2023, 11:10 AM

--- parsing/env.c
+++ parsing/env.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 11:02:42 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/04 11:02:52 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/04 11:07:21 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,40 @@ void	env_pars(t_ms *t)
 		free(temp);
 		i++;
 	}
+}
+
+void	env_var_detect(t_ms *t)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (t->cmdlist[0][i] != NULL)
+	{
+		if (ft_strnstr(t->cmdlist[0][i], "$", 1) != 0)
+		{
+			tmp = env_var(t, t->cmdlist[0][i]);
+			if (tmp != NULL)
+				t->cmdlist[0][i] = tmp;
+		}
+		i++;
+	}
+}
+
+char	*env_var(t_ms *t, char *str)
+{
+	int		i;
+
+	i = 0;
+	if (str != NULL && *str == '$')
+		str++;
+	if (str != 0 && *str == '$' && ft_isalpha(*str+1) == 0)
+		return (str);
+	while (str != 0 && t->env[i] && \
+ft_strnstr(t->env[i], str, ft_strlen(str)) == 0)
+		i++;
+	if (str == 0 || ft_strnstr(t->env[i], str, ft_strlen(str)) == 0)
+		return (0);
+	else
+		return (t->env[i] + ft_strlen(str) + 1);
 }
--- utils/env_var.c
+++ utils/env_var.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:31:40 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/11/30 11:36:21 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/04 11:10:42 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	detect_env_var(char *str)
 	quote = 0;
 	env_var = 0;
 	if (!str)
-		return 0;
+		return (0);
 	while (str[i] != '\0')
 	{
 		if (str[i] == 39 && quote == 0)
@@ -40,35 +40,26 @@ int	detect_env_var(char *str)
 
 char	*handle_env_var(t_ms *t, char *str)
 {
-	int	i;
-	int	j;
-	int	quote;
-	int	squote;
-	int	len;
-	char *var;
-	char *newstr;
+	int		quote;
+	int		squote;
+	int		len;
+	char	*var;
+	char	*newstr;
 
-	i = 0;
-	j = 0;
+	t->i = 0;
+	t->j = 0;
 	quote = 0;
 	squote = 0;
 	len = 0;
 	if (!str)
-		return NULL;
+		return (NULL);
 	if (detect_env_var(str) == 0)
 		return (str);
 	newstr = ft_calloc(ft_strlen(str) * 100, sizeof(char));
 	var = ft_calloc(100000, sizeof(char));
-	while (str[i] != '\0')
+	while (str[t->i] != '\0')
 	{
-		if (str[i] == 39 && squote == 0)
-			squote = 1;
-		if (str[i] == 34 && quote == 0)
-			quote = 1;
-		if (str[i] == 34 && quote == 1)
-			quote = 0;
-		if (str[i] == 39 && squote == 1)
-			squote = 0;
+		hev_quote(str[t->i], &squote)
 		if ((quote == 1 || (squote == 0 && quote == 0)) && str[i] == '$' && ft_isalpha(str[i+1]))
 		{
 			while(ft_isalpha(str[++i]) != 0)
@@ -87,3 +78,12 @@ char	*handle_env_var(t_ms *t, char *str)
 	free(str);
 	return (newstr);
 }
+
+		if (str[i] == 39 && squote == 0)
+			squote = 1;
+		if (str[i] == 34 && quote == 0)
+			quote = 1;
+		if (str[i] == 34 && quote == 1)
+			quote = 0;
+		if (str[i] == 39 && squote == 1)
+			squote = 0;
