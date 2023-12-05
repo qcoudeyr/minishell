33d55aa3e402327de82ba9bd7fbbd1d67913137e
Qcoudeyr <qcoudeyr@pm.me>
2023-12-05 12:58:02 +0000

    Dec 5, 2023, 1:58 PM

--- builtins/ft_unset.c
+++ builtins/ft_unset.c
@@ -6,14 +6,39 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:01:23 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/05 09:40:32 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/05 13:56:39 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	varlen_env(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL || *str == 0)
+		return (0);
+	while (str[i] != 0 && str[i] != '=')
+		i++;
+	return (i);
+}
+
 void	ft_unset(t_ms *t, int i)
 {
-	
+	int		j;
+	int		index;
+
+	j = 0;
+	index = 0;
+	while (t->cmdlist[i][j] != NULL)
+	{
+
+		while (t->env[index] != NULL)
+		{
+			if (ft_strncmp(t->env[index], t->cmdlist[i][j], varlen_env(t->cmdlist[i][j])) != 0)
+				t->env[i]
+		}
+	}
 
 }
