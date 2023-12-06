e882ad99d6194f648befb3f1d09df68bfd45175b
Qcoudeyr <qcoudeyr@pm.me>
2023-12-06 20:25:05 +0000

    Dec 6, 2023, 9:25 PM

--- main/signal.c
+++ main/signal.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 21:08:55 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/06 21:19:31 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/06 21:24:27 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,20 @@
 void	sigint_handler(int signal)
 {
 	if (signal != SIGINT)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		return ;
+	}
 	if (signal != SIGQUIT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 		return ;
+	}
 }
 
 void	handle_signal(t_ms *t)
