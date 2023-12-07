551183f387d69a289e6667d0a043cbcead1e8417
Qcoudeyr <qcoudeyr@pm.me>
2023-12-07 08:25:30 +0000

    Dec 7, 2023, 9:25 AM

--- utils/heredoc.c
+++ utils/heredoc.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 09:05:39 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/07 09:19:22 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/07 09:25:25 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,21 @@ void	handle_heredoc(t_ms *t, int index, int i)
 {
 	char	*temp;
 	int		len;
+	char	*stop;
 
 	if (t->cmdl[index][i + 1] == NULL || *t->cmdl[index][i + 1] == 0)
 		return (heredoc_error(t, index));
 	len = ft_strlen(t->cmdl[index][i + 1]);
 	temp = NULL;
-	while (ft_strnstr(temp, t->cmdl[index][i + 1], len) == 0)
+	stop = ft_strjoin(t->cmdl[index][i + 1], "\n");
+	while (ft_strnstr(temp, stop, len) == 0)
 	{
 		t->temp = temp;
 		temp = ft_strjoin(temp, get_next_line(STDIN_FILENO));
+		temp[ft_strlen(temp) - 1] = 0;
 		t->temp = pfree(t->temp);
 	}
 	write(t->input_fd, temp, ft_strlen(temp) + 1);
 	temp = pfree(temp);
+	stop = pfree(stop);
 }
