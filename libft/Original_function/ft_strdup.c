6210337adbc74f9d4adb8dc2b9677b0d83d70c97
Qcoudeyr <qcoudeyr@pm.me>
2023-12-08 09:10:34 +0000

    Dec 8, 2023, 10:10 AM

--- libft/Original_function/ft_strdup.c
+++ libft/Original_function/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:51:57 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/04/21 22:55:54 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/08 10:08:36 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s)
 
 	len = ft_strlen(s);
 	str = malloc(len + 1);
-	if (str == NULL)
+	if (str == NULL || s == NULL)
 		return (NULL);
 	ft_strlcpy(str, s, len +1);
 	return (str);
