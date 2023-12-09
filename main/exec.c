effd7d4eb0a80b96b02369d32d8b2d9599fe8756
Qcoudeyr <qcoudeyr@pm.me>
2023-12-09 11:05:06 +0000

    Dec 9, 2023, 12:05 PM

--- main/exec.c
+++ main/exec.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:42:16 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/09 12:01:47 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/09 12:05:05 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,11 @@ void	replace_index(t_ms *t, int index)
 
 int		h_nalhpa(t_ms *t)
 {
-	if (ft_strchr("<>|&", t->cmdl[t->index][0][0]))
+	if (ft_strchr("<>|&", t->cmdl[t->index][0][0]) != NULL)
+	{
+		t->temp = ft_strjoin("minishell : syntax error near unexpected token ", &t->cmdl[t->index][0][0])
+		write(0, t->temp, ft_strlen(t->temp));
+	}
 }
 
 void	exec_cmd(t_ms *t)
