87e07e0dce2e2a417a48a66726c102cabceb6090
Qcoudeyr <qcoudeyr@pm.me>
2023-12-10 19:34:38 +0100

    Fix de remote quote et handle env variable qui ne fonctionner pas si t->index > 1 car ne boucler pas dessus et fix des segfault sur plusieurs niveau de l'exec en cas de cmdl NULL

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:40:49 by lheinric          #+#    #+#             */
-/*   Updated: 2023/12/09 13:03:38 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/10 19:33:17 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ char	*spec_format(char *str)
 	str = pfree(str);
 	return (s.newstr);
 }
+
 int	check_cmd_err(t_ms *t)
 {
 	int	len;
@@ -93,10 +94,8 @@ int	cmdformat(t_ms *t)
 	int		i;
 	int		j;
 
-	j = 0;
 	i = 0;
-	t->cmd[t->nc] = rmcharq(t->cmd[t->nc], '\\');
-	t->cmd[t->nc] = spec_format(t->cmd[t->nc]);
+	t->cmd[t->nc] = spec_format(rmcharq(t->cmd[t->nc], '\\'));
 	if (t->cmd[t->nc] != NULL && *t->cmd[t->nc] != 0 && check_cmd_err(t) == 0)
 	{
 		if (*t->cmd[t->nc] == 0)
@@ -107,10 +106,12 @@ int	cmdformat(t_ms *t)
 		t->temp = pfree(t->temp);
 		if (have_pipe(t->cmdl[i]) == 1)
 			handle_pipe(t);
-		while (t->cmdl[i][j] != 0)
+		while (t->cmdl[i] != NULL)
 		{
-			t->cmdl[i][j] = remove_quotes(handle_env_var(t, t->cmdl[i][j]));
-			j++;
+			j = -1;
+			while (t->cmdl[i][++j] != NULL)
+				t->cmdl[i][j] = remove_quotes(handle_env_var(t, t->cmdl[i][j]));
+			i++;
 		}
 		return (cmd_handler(t));
 	}
