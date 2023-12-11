033fae5f64803857c5007a28267a930268e574fe
Qcoudeyr <qcoudeyr@pm.me>
2023-12-11 13:21:47 +0000

    Dec 11, 2023, 2:21 PM

--- builtins/ft_pwd.c
+++ builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:54:47 by lheinric          #+#    #+#             */
-/*   Updated: 2023/12/11 14:08:27 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/11 14:16:05 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_pwd(t_ms *t)
 	i = 0;
 	while (t->export[i] != NULL)
 	{
-		if (ft_strncmp(t->export[i], "PWD=", 3))
+		if (ft_strncmp(t->export[i], "PWD=", 3) == 0)
 		{
 			printf("%s\n", t->export[i]);
 			return (0);
